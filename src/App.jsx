4fe379794bb50805d2ffd263a5766f11dfe14ab9
FZoltan40 <farkasz@kkszki.hu>
2025-02-13 18:54:23 +0100

    Post-Get are ready to use in frontend.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css';
+import AddNewCar from './components/AddNewCar';
+import GetallCars from './components/GetallCars';
+
 
 function App()
 {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='container'>
+      <AddNewCar />
+      <GetallCars />
     </div>
-  );
+  )
 }
+
 
 export default App;
