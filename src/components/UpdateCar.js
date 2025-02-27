33946848637f2e38839dda0f506b65f657450171
Peter86 <144822322+Peter86111@users.noreply.github.com>
2025-02-27 18:23:15 +0100

    Modify UpdateCar

--- src/components/UpdateCar.js
+++ src/components/UpdateCar.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 function UpdateCar(props)
 {
-const handleCarData = () => {
-    console.log(props.carObjData)
-    console.log(props.carId)
+const handleCarData = async (event) => {
+    const url = `https://localhost:7118/cars?id=${props.carId}`
+
+    const request = await fetch(url, {
+        method: "PUT",
+        body: JSON.stringify(props.carObjData),
+        headers: {"Content-type": "Application/json"}
+    })
+
+    if(!request.ok)
+        {
+            console.log("Hiba")
+            return
+        }
+
+        var response = await request.json()
+        console.log(respone.message)
 }
 
     return (
